9c3f81e6e3c53d4416a39e8989c05352ce148015
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-08-15 10:52:48 +0200

    pages: add models

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Run:
 
     npm run dev
     make run
+
+
+## License
+This project is licensed under the **GNU General Public License v3.0**.
+See the [LICENSE](LICENSE) file for details.
--- packages/lineo-pages/src/lineo_pages/models.py
+++ packages/lineo-pages/src/lineo_pages/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
-# Create your models here.
+
+class Page(models.Model):
+    lang = models.CharField('Language', max_length=9)
+    path = models.CharField(max_length=99)
+    title = models.CharField(max_length=99)
+    description = models.CharField(max_length=999)
+
+
+class Block(PolymorphicModel):
+    page = models.ForeignKey(Page, models.CASCADE)
+
+    class Meta:
+        order_with_respect_to = 'page'
+
+
+class Heading(Block):
+
+    LEVEL_CHOICES = [(i, i) for i in range(1, 7)]
+
+    content = models.CharField(max_length=99)
+    level = models.PositiveSmallIntegerField(choices=LEVEL_CHOICES)
+
+
+class Text(Block):
+    content = models.CharField(max_length=99)
